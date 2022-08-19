e45585edcb7c52243f5be6fe424478c98c2f5e98
flaviaclara <flavia.cj18@gmail.com>
2022-08-19 11:15:27 -0300

    criado uma classe com nome Hello Word

---
+++ Hello Word/src/hello/word/HelloWord.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.word;
+
+/**
+ *
+ * @author 822160517
+ */
+public class HelloWord {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello 'Word");
+        // TODO code application logic here
+        
+    }
+    
+}
